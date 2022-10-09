9b04e81ab32f39a4e5893f0fe66b033f20a18482
imsk17 <imsumitkumar17@gmail.com>
2022-10-09 20:56:33 +0530

    :bug: Ignore Version File Errors

--- version.ts
+++ version.ts
@@ -1,12 +1,15 @@
+// deno-lint-ignore-file require-await
 /** `version` managed by https://deno.land/x/land/publish. */
-export const VERSION = "0.0.1"
+export const VERSION = "0.0.1";
 
 /** `prepublish` will be invoked before publish, return `false` to prevent the publish. */
-export async function prepublish(version: string) {
-  return true
+//@ts-ignore: unused
+export async function prepublish(_version: string) {
+  return true;
 }
 
 /** `postpublish` will be invoked after published. */
+//@ts-ignore: unused
 export async function postpublish(version: string) {
-  console.log("Upgraded to", version)
+  console.log("Upgraded to", version);
 }
